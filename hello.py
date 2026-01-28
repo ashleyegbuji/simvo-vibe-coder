de14467ea5e24beece23b83571818bd235be3915
Ashley Egbuji <nkemegbuji@gmail.com>
2026-01-28 12:25:09 -0800

    Added first hello program

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+try:
+        while True:  # Step 2: infinite loop
+            pass  # Step 3: do nothing, just wait
+except KeyboardInterrupt:
+        print("\nGoodbye, user!")
