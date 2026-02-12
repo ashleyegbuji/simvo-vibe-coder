5be6952b83a9e644ee53b00af38ad7d4e94d6c0b
Ashley Egbuji <nkemegbuji@gmail.com>
2026-02-12 14:07:12 -0800

    added html route

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from request import fetch_species
 
-app = FastAPI()
+app = FastAPI(debug=True)
+templates = Jinja2Templates(directory="templates")
 
+# Home page
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/hello")
-def hello():
-    return {"message": "Hello, world!"}
-
+# Species API
 @app.get("/species")
-def get_species(name: str = Query(..., description="Name of the species")):
-    print("i got: " + search)
-    return fetch_species(name, per_page)
+def species_api(name: str = Query(...)):
+    data = fetch_species(name)
+    return data
+
+# Status route
+@app.get("/status")
+def status():
+    return {"message": "Server is running!"}
